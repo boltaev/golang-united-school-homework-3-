67120102e43e40ba9a9661cbf17d34fc51a37823
lazizbek <74559830+boltaev@users.noreply.github.com>
2022-06-07 02:37:30 +0500

    resolve task03

--- task02-slice.go
+++ task02-slice.go
@@ -1,10 +1,10 @@
 package homework
 
-func reverse(input []int64) (result []int64) {
-	//Place your code here
+func reverse(input []int64) []int64 {
+	result := make([]int64, len(input))
 	copy(result, input)
 	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
 		result[i], result[j] = result[j], result[i]
 	}
-	return
+	return result
 }
