1dae23feb7eaad8dbd361237d4d7a310074736f1
lazizbek <74559830+boltaev@users.noreply.github.com>
2022-06-07 02:17:01 +0500

    solve task03

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,24 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	//saving keys of map into slice
+	keys := make([]int, len(input))
+	i := 0
+	for k := range input {
+		keys[i] = k
+		i++
+	}
+
+	// sorting keys
+	sort.Ints(keys)
+
+	//saving values of map into slice
+	for _, k := range keys {
+		result = append(result, input[k])
+	}
+
+	return result
 }
